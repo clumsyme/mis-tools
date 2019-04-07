d442a7871cdf1c603168e1fd781f995ad8ad81d6
LiYan <lidoyan@gmail.com>
2019-04-07 22:52:23 +0800

    使用内置 prettier

--- src/format.ts
+++ src/format.ts
@@ -1,16 +1,23 @@
 import * as vscode from 'vscode'
 import { exec } from 'child_process'
+import * as prettier from 'prettier'
 
 const window = vscode.window
 
+function fullDocumentRange(document: vscode.TextDocument): vscode.Range {
+    const lastLineId = document.lineCount - 1
+    return new vscode.Range(0, 0, lastLineId, document.lineAt(lastLineId).text.length)
+}
+
 export function onFormat() {
-    let currentFilePath
+    let currentFilePath: string
     try {
         currentFilePath = vscode.window.activeTextEditor.document.fileName
     } catch (error) {
         vscode.window.showInformationMessage('当前没有打开的 JavaScript 文件')
         return
     }
+
     window.withProgress(
         {
             location: vscode.ProgressLocation.Notification,
@@ -18,12 +25,20 @@ export function onFormat() {
         },
         (progress, token) => {
             return new Promise((resolve) => {
-                exec(
-                    `npx prettier --single-quote --no-semi --trailing-comma all --arrow-parens always --tab-width 4 --print-width 100 --write ${currentFilePath}`,
-                    () => {
-                        resolve()
-                    },
-                )
+                let opendDocument: vscode.TextDocument = vscode.window.activeTextEditor.document
+                let formatted = prettier.format(opendDocument.getText(), {
+                    parser: 'babel',
+                    semi: false,
+                    singleQuote: true,
+                    trailingComma: 'all',
+                    arrowParens: 'always',
+                    tabWidth: 4,
+                    printWidth: 100,
+                })
+                const edit = new vscode.WorkspaceEdit()
+                edit.replace(opendDocument.uri, fullDocumentRange(opendDocument), formatted)
+                vscode.workspace.applyEdit(edit)
+                resolve()
             })
         },
     )
